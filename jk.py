0a6a3db8f5cd83ad6ac1036589404a5cfd6c0eec
aaaaninja <aaaaninja@aaaa.ninja>
2020-04-26 22:02:18 +0900

    単純に動かそうとすると `frozen=True` を外さないといけない

--- jk.py
+++ jk.py
@@ -1,13 +1,13 @@
 import dataclasses
 import typing
 
-@dataclasses.dataclass(frozen=True)
+@dataclasses.dataclass #(frozen=True)
 class FuelClient:
     clientsecret: dataclasses.InitVar[str]
     clientid: dataclasses.InitVar[str]
     et_folder_client: str = dataclasses.field(init=False)
-    def __post_init__(self):
-        self.et_folder_client = self.clientid + self.clientsecret
+    def __post_init__(self, clientid, clientsecret):
+        self.et_folder_client = clientid + clientsecret
         
     
     def ET_Folder(self) -> str:
