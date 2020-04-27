b4ee427985f8fdf2a34d31b71629ff98b6912c56
aaaaninja <aaaaninja@aaaa.ninja>
2020-04-27 16:56:06 +0900

    はい (静的)

---
+++ jktyping.py
@@ -0,0 +1,23 @@
+import typing
+from dataclasses import dataclass
+from typing_extensions import Protocol
+
+class HasName(Protocol):
+    name: str
+
+@dataclass(frozen=True)
+class Person():
+    name: str
+
+    def hoge(self):
+        return self.name
+
+
+def get_name(o: HasName) -> str:
+    return o.name
+
+if __name__ == "__main__":
+    print(Person(name='asdfasdf'))
+    print(Person(name='asdfasdf').name)
+
+    print(get_name(Person(name='asdfasdf')))
